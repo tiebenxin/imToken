3dd2f5790df9827ed7d9da3d77f09ac42f3bc141
Liszt <1062678603@qq.com>
2021-05-11 22:39:43 +0800

    add resource

--- app/src/main/java/com/google/imtoken/MainActivity.kt
+++ app/src/main/java/com/google/imtoken/MainActivity.kt
@@ -2,11 +2,34 @@ package com.google.imtoken
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import jxl.Workbook
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        readExcel()
+    }
+
+
+    private fun readExcel() {
+        try {
+            val book =
+                Workbook.getWorkbook(assets.open("0xdbb7e5bf95d58c067f50f4c36fbebb4dfb837913.csv"))
+            val sheet = book.getSheet(0)
+            val sheetNum = book.numberOfSheets
+            val sheetRows = sheet.rows
+            val sheetColunms = sheet.columns
+            println("sheetNum=${sheetNum}")
+            println("sheetRows=${sheetRows}")
+            println("sheetColunms=${sheetColunms}")
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/google/imtoken/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.google.imtoken
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    var handler = Handler(Looper.getMainLooper())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        handler.postDelayed(Runnable {
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        }, 500)
+    }
+}
